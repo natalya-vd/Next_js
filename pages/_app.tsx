ba54875f68ae8736e36d8934c919515143bb11df
Natalya <natasha-vd@yandex.ru>
2022-08-15 23:12:16 +0300

    add fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
+
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>MyTop - приложение</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;

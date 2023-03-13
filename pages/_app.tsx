98cc8ced8a3f1bf5b8e1f6abca5c8cdea9565480
Natalya <natasha-vd@yandex.ru>
2023-03-13 14:21:08 +0300

    Husky

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
 import Head from 'next/head';
+import ym from 'react-yandex-metrika';
+import { YMInitializer } from 'react-yandex-metrika';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+  router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+      ym('hit', url);
+    }
+  });
+
   return <>
     <Head>
       <title>MyTop - приложение</title>
@@ -11,6 +19,11 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
       <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
       <meta property='og:local' content="ru_RU" />
     </Head>
+    <YMInitializer
+      accounts={[]}
+      options={{ webvisor: true, defer: true }}
+      version="2"
+    />
     <Component {...pageProps} />
   </>;
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
         </Head>
+        <link rel="preconnect" href="https://mc.yandex.ru" />
         <body>
           <Main />
           <NextScript />
